fa3090d42f3ac28664be7e8084667787248e63f7
Song0129 <Song_3191@163.com>
2022-09-14 11:11:12 +0800

    feat: pixi 往复运动

--- src/views/Pixi/Pixi.vue
+++ src/views/Pixi/Pixi.vue
@@ -7,45 +7,59 @@
 
 <script>
 	import * as PIXI from "pixi.js";
-	import { onMounted } from "vue";
+	import { onMounted, onUnmounted } from "vue";
 	export default {
 		setup() {
-			onMounted(() => {
-				// 创建一个Pixi 应用
-				let app = new PIXI.Application({ width: 460, height: 764 });
+			// 创建一个Pixi 应用
+			let app = new PIXI.Application({ width: 460, height: 764 });
 
-				const box = new PIXI.Container();
-				app.stage.addChild(box);
+			const box = new PIXI.Container();
+			app.stage.addChild(box);
 
-				const rect = new PIXI.Graphics();
-				rect.beginFill(0xff0000);
-				rect.drawRect(0, 0, 50, 50);
-				rect.endFill();
-				rect.interactive = true;
-				rect.on("pointertap", () => {
-					console.log("click");
-					// app.ticker.remove(handleTicker);
-					rect.x++;
-					rect.y++;
-				});
+			const rect = new PIXI.Graphics();
+			rect.beginFill(0xff0000);
+			rect.drawRect(0, 0, 50, 50);
+			rect.endFill();
+			rect.interactive = true;
+			rect.on("pointertap", () => {
+				console.log("click");
+				app.ticker.remove(handleTicker);
+				rect.x++;
+				rect.y++;
+			});
 
-				rect.x = 100;
-				rect.y = 100;
+			// rect.x = 100;
+			// rect.y = 100;
 
-				function handleTicker() {
-					console.log("add");
-					box.x++;
+			let speed = 1;
+			let fspeed;
+			function handleTicker() {
+				// console.log("handleTicker", rect.x);
+				if (rect.x >= 100) {
+					fspeed = -speed;
 				}
+				if (rect.x <= 0) {
+					fspeed = speed;
+				}
+				console.log(fspeed);
+				rect.x += fspeed;
+				rect.y += fspeed * 2;
+			}
 
-				app.stage.addChild(rect);
-				console.log(rect);
+			app.ticker.add(handleTicker);
+			app.stage.addChild(rect);
 
-				const text = new PIXI.Text("hello pixijs", { fill: "red" });
-				box.addChild(text);
+			const text = new PIXI.Text("hello pixijs", { fill: "red" });
+			box.addChild(text);
 
+			onMounted(() => {
 				// 把 Pixi 应用中创建出来的 canvas 添加到页面上
 				let container = document.querySelector(".container");
 				container.append(app.view);
+			});
+
+			onUnmounted(() => {
+				app.ticker.remove(handleTicker);
 			});
 
 			return {};
